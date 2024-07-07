b2e18a4681a9e19da94a544a0d8040cad5772b77
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-07-07 12:15:46 +0530

    Fake Vehicle data

--- vehicle.py
+++ vehicle.py
@@ -16,28 +16,26 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 def vehicle_data(num_vehicle):
     vehicle = []
-    for VehicleID in range(1, num_vehicle + 1):
+    for VehicleID in range(61, num_vehicle + 1):
         VehicleType = fake.vehicle_category()
         VehicleCompany = fake.vehicle_make()
         VehicleName = fake.vehicle_model()
         VehicleNumber = rstr.xeger(r"MP[0-6][0-9][A-Z]{2}\d{4}")
-        vehicle.append((VehicleID, VehicleType, VehicleCompany, VehicleName, VehicleNumber))
+        vehicle.append((VehicleID, VehicleType, VehicleNumber))
     return vehicle
 def insertVehicleDataIntoMysql(vehicle):
 #    cursor.execute('DROP TABLE IF EXISTS vehicle')
     cursor.execute('''CREATE TABLE IF NOT EXISTS vehicle( 
           `VehicleID` int NOT NULL AUTO_INCREMENT,
           `VehicleType` varchar(15) DEFAULT NULL,
-          `Company` varchar(35) DEFAULT NULL,
-          `Name` varchar(35) DEFAULT NULL,
           `VehicleNumber` varchar(35) DEFAULT NULL,
           PRIMARY KEY (`VehicleID`)
         )''')
-    insert_query = '''INSERT INTO vehicle (VehicleID, VehicleType, VehicleCompany, VehicleName, VehicleNumber) VALUES(%s,%s,%s,%s,%s)'''
+    insert_query = '''INSERT INTO vehicle (VehicleID, VehicleType,  VehicleNumber) VALUES(%s,%s,%s)'''
     cursor.executemany(insert_query, vehicle)
     conn.commit()
 
-num_vehicle  = 64 
+num_vehicle  = 60 
 vehicle_data1 = vehicle_data(num_vehicle)
 insertVehicleDataIntoMysql(vehicle_data1)
 cursor.close()
